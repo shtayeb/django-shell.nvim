b9767c9540a4ab9db6cca18438b4f4d559cbaf24
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-12-15 23:27:15 +0430

    WIP

--- lua/django-shell/init.lua
+++ lua/django-shell/init.lua
@@ -3,6 +3,17 @@ local M = {}
 function M.setup(opts)
 	opts = opts or {}
 
+	vim.keymap.set("n","<space>tr",function ()
+		vim.cmd.vnew()
+		vim.cmd.term()
+		vim.cmd.wincmd("J")
+		vim.api.nvim_win_set_height(0,10)
+
+		local job_id = vim.bo.channel
+
+		vim.fn.chansend(job_id, {"echo hi \r\n"})
+	end)
+
 	vim.keymap.set("n", "<Leader>h", function()
 		if opts.name then
 			print("Hello, " .. opts.name)
