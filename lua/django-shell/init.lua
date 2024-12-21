7ef31b8f3f1a648dfbd5fb46359683c10e40fb5e
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-12-21 22:03:26 +0430

    telescopt pickers

--- lua/django-shell/init.lua
+++ lua/django-shell/init.lua
@@ -1,4 +1,38 @@
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local config = require("telescope.config").values
+local previewers = require("telescope.previewers")
+local tels_prevs_utils = require("telescope.previewers.utils")
+
 local M = {}
+
+M.show_django_cmds = function(opts)
+	pickers
+		.new(opts, {
+			finder = finders.new_table({
+				results = {
+					{ name = "Yes", value = { 1, 2, 3 } },
+					{ name = "No", value = { -1, -2, -3 } },
+				},
+				entry_maker = function(entry)
+					return {
+						value = entry,
+						display = entry.name,
+						ordinal = entry.name, -- is the sorting and filtering key
+					}
+				end,
+			}),
+			sorter = config.generic_sorter(opts),
+			previewer = previewers.new_buffer_previewer({
+				title = "Command Help",
+				define_preview = function(self, entry)
+					vim.api.nvim_buf_set_lines(self.state.bufnr, 0, 0, false, vim.split(vim.inspect(entry.value), "\n"))
+					tels_prevs_utils.highlighter(self.state.bufnr, "python")
+				end,
+			}),
+		})
+		:find()
+end
 
 M.exec_django_code = function(code)
 	-- execute the django code and return the result
@@ -27,6 +61,10 @@ function M.setup(opts)
 		-- put the result in the new buffer inside the new window
 		-- TODO: Highlight the result
 		vim.api.nvim_buf_set_lines(0, 0, 0, false, res)
+	end)
+
+	vim.keymap.set("n", "<space>tc", function()
+		M.show_django_cmds()
 	end)
 
 	-- TODO: get the available django commands and display it in a telescope search prompt
