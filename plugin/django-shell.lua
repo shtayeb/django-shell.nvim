7183d523491a32cabf7a4bbed2081ab575ce75c6
Shahryar Tayeb <shahryar.dev@gmail.com>
2025-01-03 23:12:11 +0430

    register DjangoShellExec command only for py files

--- plugin/django-shell.lua
+++ plugin/django-shell.lua
@@ -2,6 +2,11 @@ vim.api.nvim_create_user_command("DjangoCommands", function()
    require("django-shell").show_django_cmds()
 end, {})
 
-vim.api.nvim_create_user_command("DjangoShellExec", function()
-   require("django-shell").exec_django_code()
-end, {})
+vim.api.nvim_create_autocmd("FileType", {
+   pattern = "python",
+   callback = function()
+      vim.api.nvim_create_user_command("DjangoShellExec", function()
+         require("django-shell").exec_django_code()
+      end, {})
+   end,
+})
